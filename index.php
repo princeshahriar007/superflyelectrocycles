ca54bd112783db1e3ce9dbc2dfd4f3005777b2e9
Prince Shahriar <princeshahriar2013@live.com>
2020-12-24 23:00:16 +0600

    File Title Changed

--- index.php
+++ index.php
@@ -48,16 +48,17 @@
     <!-- ======= Header ======= -->
     <header id="header" class="fixed-top d-lg-none">
       <div class="container d-flex align-items-center">
+        <!-- 
         <h1 class="logo mr-auto">
           <a href="index.html">BizLand<span>.</span></a>
         </h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
+        Uncomment below if you prefer to use an image logo -->
+        <a href="index.html" class="logo mr-auto"><img src="assets/img/logo-shadowd.png" alt=""></a>-->
 
         <nav class="nav-menu d-none d-lg-block">
           <ul>
             <li class="active"><a href="index.html">Home</a></li>
-            <li><a href="#services">Services</a></li>
+            <li><a href="#services">Payment</a></li>
           </ul>
         </nav>
         <!-- .nav-menu -->
